ff326efb3320f9041f129942bab405364f1bfc61
nayanakumarirathnayake <nayanakumarirathnayake71@gmail.com>
2021-11-10 14:16:11 +0530

    addd cors commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 
 mongoose.connect(
@@ -17,7 +18,7 @@ mongoose.connect(
     console.log(err);
 })
 
-
+app.use(cors())
 app.use(express.json())
 app.use('/api', require('./routes/school'))
 app.use('/api', require('./routes/student'))
